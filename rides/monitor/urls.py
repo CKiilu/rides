bae2aa12346d3b4134e515df084f89dc7f27e2dd
Christopher Kiilu <cndeti@gmail.com>
2016-06-05 12:13:34 +0000

    Map and link fixes

--- rides/monitor/urls.py
+++ rides/monitor/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^profile/$', profile, name='profile'),
+    url(r'^history/$', history, name='history'),
     url(r'^coords/$', coords, name='coords'),
 ]
--- rides/monitor/views.py
+++ rides/monitor/views.py
@@ -2,6 +2,7 @@ from rauth import OAuth2Service
 import requests
 import simplejson
 
+from django.core.cache import cache
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
@@ -19,64 +20,90 @@ PERMISSION_SCOPES = [
     'history',
     'places',
     ]
-REDIRECT_URL = 'https://rides-ckiilu.c9users.io/profile'
+HISTORY_REDIRECT_URL = 'https://rides-ckiilu.c9users.io/history'
+PROFILE_REDIRECT_URL = 'https://rides-ckiilu.c9users.io/profile'
 SERVER_TOKEN = '41ti1oAYb5wcLWKTUjgxG-B4grRYeYQlrVzoTCkf'
+test_url ="https://sandbox-api.uber.com/v1/"
+prod_url = 'https://api.uber.com/v1/',
+
+uber_api = OAuth2Service(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    name='FuelMonitor',
+    authorize_url='https://login.uber.com/oauth/authorize',
+    access_token_url='https://login.uber.com/oauth/token',
+    base_url=test_url
+)
 
 # Redirect user here to authorize your application
 
 
 def home(request):
-    uber_api = OAuth2Service(
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        name='FuelMonitor',
-        authorize_url='https://login.uber.com/oauth/authorize',
-        access_token_url='https://login.uber.com/oauth/token',
-        base_url='https://api.uber.com/v1/',
-    )
-    
+    prof_url = None
     parameters = {
         'response_type': 'code',
-        'redirect_uri': REDIRECT_URL,
+        'redirect_uri': PROFILE_REDIRECT_URL,
         'scopes': PERMISSION_SCOPES,
     }
-    login_url = uber_api.get_authorize_url(**parameters)
+    hist_parameters = {
+        'response_type': 'code',
+        'redirect_uri': HISTORY_REDIRECT_URL,
+        'scopes': PERMISSION_SCOPES,
+    }
+    prof_url = uber_api.get_authorize_url(**parameters)
+    hist_url = uber_api.get_authorize_url(**hist_parameters)
+    
     context = {
-        'login': login_url,
+        'profile': prof_url,
+        'history': hist_url
     }
     return render(request, 'home.html', context)
     
 def profile(request):
     data = None
     if request.GET.get('code'):
-        parameters = {
-            'redirect_uri': REDIRECT_URL,
-            'code': request.GET.get('code'),
-            'grant_type': 'authorization_code',
-        }
-        
-        response = requests.post(
-            'https://login.uber.com/oauth/token',
-            auth=(
-                CLIENT_ID,
-                CLIENT_SECRET,
-            ),
-            data=parameters,
-        )
-        access_token = response.json().get('access_token')
-        url = 'https://api.uber.com/v1/me'
-        response = requests.get(
-            url,
-            headers={
-                'Authorization': 'Bearer %s' % access_token
-            }
-        )
-        data = response.json()
+        data = uber_auth(request, test_url + 'me', PROFILE_REDIRECT_URL)
     
     context = {
         'profile': data
     }
     return render(request, 'detail.html', context)
+    
+def history(request):
+    data = None
+    if request.GET.get('code'):
+        data = uber_auth(request, test_url[:len(test_url) - 1] + '.2/history', HISTORY_REDIRECT_URL)
+    
+    context = {
+        'history': data
+    }
+    return render(request, 'history.html', context)
+    
+def uber_auth(request, url, redirected_url):
+    parameters = {
+        'redirect_uri': redirected_url,
+        'code': request.GET.get('code'),
+        'grant_type': 'authorization_code',
+    }
+    
+    response = requests.post(
+        'https://login.uber.com/oauth/token',
+        auth=(
+            CLIENT_ID,
+            CLIENT_SECRET,
+        ),
+        data=parameters,
+    )
+    access_token = response.json().get('access_token')
+    response = requests.get(
+        url,
+        headers={
+            'Authorization': 'Bearer %s' % access_token
+        }
+    )
+    
+    return response.json()
+        
     
 def coords(request):
     if request.is_ajax():
@@ -90,16 +117,15 @@ def coords(request):
                 'end_longitude': request.GET.get('end[lat]'),
             }
             
-            url = 'https://api.uber.com/v1/estimates/time' if req_type == 'time' else 'https://api.uber.com/v1/estimates/price'
+            url = 'estimates/time' if req_type == 'time' else 'estimates/price'
         elif req_type == 'products':
             PARAMETERS = {
                 'server_token': SERVER_TOKEN,
                 'latitude': request.GET.get('end[lat]'),
                 'longitude': request.GET.get('end[lng]'),
             }
-            url = 'https://api.uber.com/v1/products'
-        response = requests.get(url, params=PARAMETERS)
-        print req_type
+            url = 'products'
+        response = requests.get(test_url + url, params=PARAMETERS)
         data = response.text
         json = simplejson.dumps(data)
         return HttpResponse(json, 'application/json')
